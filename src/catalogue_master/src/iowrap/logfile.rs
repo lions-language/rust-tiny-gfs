3f3c22f1e70f02537a36a7a47ba0b0980ed2c4cb
lions-language <lions_language@163.com>
2022-06-11 09:25:52 +0800

    update

--- src/catalogue_master/src/iowrap/logfile.rs
+++ src/catalogue_master/src/iowrap/logfile.rs
@@ -1,14 +1,22 @@
 use std::fs::File;
 use std::path::Path;
 
+use bytes::{Buf, BufMut};
+
 use crate::{Error, Result};
 
+/*
+ * 1. write: line
+ * 2. read: aggr
+ * */
 pub struct LogFile {
     file: File,
 }
 
 impl LogFile {
-    fn load(&mut self) {}
+    pub fn write(&mut self, bytes: &impl Buf) {}
+
+    pub fn read(&mut self, output: &mut impl BufMut) {}
 
     pub fn open(path: impl AsRef<Path>) -> Result<Self> {
         Ok(Self {
--- src/catalogue_master/src/lib.rs
+++ src/catalogue_master/src/lib.rs
@@ -1,6 +1,7 @@
 mod error;
 mod idgenerator;
 mod iowrap;
+mod serdeser;
 mod storage;
 
 pub(crate) use error::{Error, Result};
