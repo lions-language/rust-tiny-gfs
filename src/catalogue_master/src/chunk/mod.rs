fbb793d05396e036d9285f14f64fad8c28e69177
lions-language <lions_language@163.com>
2022-06-21 21:55:05 +0800

    update

--- src/catalogue_master/src/chunk/mod.rs
+++ src/catalogue_master/src/chunk/mod.rs
@@ -5,11 +5,19 @@ pub use storage::StorageMode;
 use crate::Result;
 use storage::{Storage, StorageFactory};
 
-pub struct ChunkService {
+pub struct ChunkHandler {
     storage: Box<dyn Storage>,
 }
 
-impl ChunkService {
+impl ChunkHandler {
+    pub fn start(&mut self) -> Result<()> {
+        unimplemented!();
+    }
+
+    fn handle_heartbeat(&mut self) -> Result<()> {
+        unimplemented!();
+    }
+
     pub fn new(storage_mode: StorageMode) -> Result<Self> {
         Ok(Self {
             storage: StorageFactory::new_storage(storage_mode)?,
