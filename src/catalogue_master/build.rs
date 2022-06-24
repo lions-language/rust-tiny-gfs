8789d2e245d87c5dd5dc3ad4a0fba1b5a47c0254
lions-language <lions_language@163.com>
2022-06-24 09:44:37 +0800

    update

---
+++ src/catalogue_master/build.rs
@@ -0,0 +1,15 @@
+use std::io::Result;
+
+fn main() -> Result<()> {
+    let out_dir = "src/proto";
+
+    let _ = std::fs::create_dir(out_dir);
+
+    tonic_build::configure()
+        .build_server(false)
+        .out_dir(out_dir)
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile(&["chunk_handler_service.proto"], &["../../protos"])?;
+
+    Ok(())
+}
