80b2029619e8e136ebd750abeb0067b92efa693d
lions-language <lions_language@163.com>
2022-07-01 22:54:13 +0800

    update

--- src/catalogue_master/src/chunk/mod.rs
+++ src/catalogue_master/src/chunk/mod.rs
@@ -11,6 +11,7 @@ use tokio_stream::{wrappers::ReceiverStream, StreamExt};
 use tonic::{transport::Server, Request, Response, Status, Streaming};
 
 use std::sync::Arc;
+use std::{error::Error, io::ErrorKind};
 use std::{pin::Pin, time::Duration};
 
 // pub mod chunk_handler {
@@ -18,6 +19,29 @@ use std::{pin::Pin, time::Duration};
 // }
 
 use crate::proto::chunk_handler::{chunk_handler_service_server::*, *};
+
+fn match_for_io_error(err_status: &Status) -> Option<&std::io::Error> {
+    let mut err: &(dyn Error + 'static) = err_status;
+
+    loop {
+        if let Some(io_err) = err.downcast_ref::<std::io::Error>() {
+            return Some(io_err);
+        }
+
+        // h2::Error do not expose std::io::Error with `source()`
+        // https://github.com/hyperium/h2/pull/462
+        if let Some(h2_err) = err.downcast_ref::<h2::Error>() {
+            if let Some(io_err) = h2_err.get_io() {
+                return Some(io_err);
+            }
+        }
+
+        err = match err.source() {
+            Some(err) => err,
+            None => return None,
+        };
+    }
+}
 
 type HeartbeatResponseStream =
     Pin<Box<dyn Stream<Item = std::result::Result<HeartbeatResponse, Status>> + Send>>;
@@ -45,40 +69,76 @@ impl ChunkHandlerService for ChunkHandlerServiceImpl {
     type heartbeatStream =
         Pin<Box<dyn Stream<Item = std::result::Result<HeartbeatResponse, Status>> + Send>>;
 
+    // async fn heartbeat(
+    //     &self,
+    //     request: Request<Streaming<HeartbeatRequest>>,
+    // ) -> std::result::Result<Response<Self::heartbeatStream>, Status> {
+    //     println!("\tclient connected from: {:?}", request.remote_addr());
+
+    //     // creating infinite stream with requested message
+    //     let repeat = std::iter::repeat(HeartbeatResponse {
+    //         // message: request.into_inner().message,
+    //     });
+    //     let mut stream = Box::pin(tokio_stream::iter(repeat).throttle(Duration::from_millis(200)));
+
+    //     // spawn and channel are required if you want handle "disconnect" functionality
+    //     // the `out_stream` will not be polled after client disconnect
+    //     let (tx, rx) = mpsc::channel(128);
+    //     tokio::spawn(async move {
+    //         while let Some(item) = stream.next().await {
+    //             match tx.send(std::result::Result::<_, Status>::Ok(item)).await {
+    //                 Ok(_) => {
+    //                     // item (server response) was queued to be send to client
+    //                 }
+    //                 Err(_item) => {
+    //                     // output_stream was build from rx and both are dropped
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //         println!("\tclient disconnected");
+    //     });
+
+    //     let output_stream = ReceiverStream::new(rx);
+    //     Ok(Response::new(
+    //         Box::pin(output_stream) as Self::heartbeatStream
+    //     ))
+    // }
+
     async fn heartbeat(
         &self,
         request: Request<Streaming<HeartbeatRequest>>,
     ) -> std::result::Result<Response<Self::heartbeatStream>, Status> {
         println!("\tclient connected from: {:?}", request.remote_addr());
 
-        // creating infinite stream with requested message
-        let repeat = std::iter::repeat(HeartbeatResponse {
-            // message: request.into_inner().message,
-        });
-        let mut stream = Box::pin(tokio_stream::iter(repeat).throttle(Duration::from_millis(200)));
-
-        // spawn and channel are required if you want handle "disconnect" functionality
-        // the `out_stream` will not be polled after client disconnect
+        let mut in_stream = request.into_inner();
         let (tx, rx) = mpsc::channel(128);
+
         tokio::spawn(async move {
-            while let Some(item) = stream.next().await {
-                match tx.send(std::result::Result::<_, Status>::Ok(item)).await {
-                    Ok(_) => {
-                        // item (server response) was queued to be send to client
-                    }
-                    Err(_item) => {
-                        // output_stream was build from rx and both are dropped
-                        break;
+            while let Some(item) = in_stream.next().await {
+                match item {
+                    Ok(v) => tx.send(Ok(HeartbeatResponse {})).await.expect("working rx"),
+                    Err(err) => {
+                        if let Some(io_err) = match_for_io_error(&err) {
+                            if io_err.kind() == ErrorKind::BrokenPipe {
+                                eprintln!("client disconnected: broken pipe");
+                                break;
+                            }
+                        }
+
+                        match tx.send(Err(err)).await {
+                            Ok(_) => (),
+                            Err(_err) => break,
+                        }
                     }
                 }
             }
-            println!("\tclient disconnected");
+            println!("stream ended");
         });
 
-        let output_stream = ReceiverStream::new(rx);
-        Ok(Response::new(
-            Box::pin(output_stream) as Self::heartbeatStream
-        ))
+        let out_stream = ReceiverStream::new(rx);
+
+        Ok(Response::new(Box::pin(out_stream) as Self::heartbeatStream))
     }
 }
 
