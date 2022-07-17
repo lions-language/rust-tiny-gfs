069cd2ee91ed071d69e6d518e717a193281f92e3
lions-language <lions_language@163.com>
2022-07-17 13:55:18 +0800

    update

--- src/catalogue_master/src/chunk/operator.rs
+++ src/catalogue_master/src/chunk/operator.rs
@@ -1,12 +1,20 @@
 use super::ArcStorage;
+use crate::ChunkId;
 
 pub(crate) struct ChunkOperator {
     storage: ArcStorage,
 }
 
+pub(crate) struct ApplyChunkData {
+    num_chunks: usize,
+}
+
 impl ChunkOperator {
+    pub(crate) fn apply_chunks(&mut self, data: ApplyChunkData) -> Vec<ChunkId> {
+        unimplemented!();
+    }
+
     pub(crate) fn new(storage: ArcStorage) -> Self {
         Self { storage: storage }
     }
 }
-
