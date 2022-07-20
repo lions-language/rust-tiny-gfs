0dcb5168a6ab464acb43eb302ab07f554fd41e67
lions-language <lions_language@163.com>
2022-07-20 22:14:18 +0800

    update

--- src/catalogue_master/src/server/allocer/mod.rs
+++ src/catalogue_master/src/server/allocer/mod.rs
@@ -4,7 +4,13 @@ pub(crate) struct Allocer {}
 
 impl Allocer {
     pub(crate) fn alloc(&mut self) -> Result<Chunk> {
-        // 1.
+        // 1. file is exists (read from metadata)
+        // - exist:
+        //  - read finish marker / init state
+        //      - finish: return error
+        // - not exist:
+        //  - create metadata
+        unimplemented!();
     }
 
     pub(crate) fn new() -> Self {
