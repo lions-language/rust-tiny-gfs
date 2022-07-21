bacf5f8ba1e4c4efed95ae31c5cb4b0ce938fb60
lions-language <lions_language@163.com>
2022-07-21 10:06:57 +0800

    update

--- src/catalogue_master/src/server/allocer/mod.rs
+++ src/catalogue_master/src/server/allocer/mod.rs
@@ -3,12 +3,14 @@ use crate::{Chunk, Result};
 pub(crate) struct Allocer {}
 
 impl Allocer {
-    pub(crate) fn alloc(&mut self) -> Result<Chunk> {
+    pub(crate) fn alloc(&mut self) -> Result<Vec<Chunk>> {
         // 1. file is exists (read from metadata)
         // - exist:
-        //  - read finish marker / init state
+        //  - read finish marker
         //      - finish: return error
+        //      - init state / writing state: return success
         // - not exist:
+        //  - alloc balance chunks
         //  - create metadata
         unimplemented!();
     }
