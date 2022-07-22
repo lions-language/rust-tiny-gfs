b6ba11cc6d0c4d5d59c427ab5fea7c021f71f64e
lions-language <lions_language@163.com>
2022-07-22 17:49:43 +0800

    update

--- src/catalogue_master/tests/chunk_handler_heartbeat/src/main.rs
+++ src/catalogue_master/tests/chunk_handler_heartbeat/src/main.rs
@@ -2,8 +2,48 @@ use catalogue_master::proto::chunk_handler::{chunk_handler_service_client::*, *}
 
 use futures::stream::Stream;
 use std::time::Duration;
-use tokio_stream::StreamExt;
+use tokio_stream::{wrappers::IntervalStream, StreamExt};
 use tonic::transport::Channel;
+
+pub struct HeartbeatStream {
+    inner: Heartbeat,
+}
+
+impl HeartbeatStream {
+    /// Create a new `HeartbeatStream`.
+    pub fn new(interval: Heartbeat) -> Self {
+        Self { inner: interval }
+    }
+
+    /// Get back the inner `Heartbeat`.
+    pub fn into_inner(self) -> Heartbeat {
+        self.inner
+    }
+}
+
+impl Stream for HeartbeatStream {
+    type Item = Instant;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Instant>> {
+        self.inner.poll_tick(cx).map(Some)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (std::usize::MAX, None)
+    }
+}
+
+impl AsRef<Heartbeat> for HeartbeatStream {
+    fn as_ref(&self) -> &Heartbeat {
+        &self.inner
+    }
+}
+
+impl AsMut<Heartbeat> for HeartbeatStream {
+    fn as_mut(&mut self) -> &mut Heartbeat {
+        &mut self.inner
+    }
+}
 
 fn heartbeat_requests_iter() -> impl Stream<Item = HeartbeatRequest> {
     tokio_stream::iter(1..usize::MAX).map(|i| HeartbeatRequest {
@@ -12,7 +52,9 @@ fn heartbeat_requests_iter() -> impl Stream<Item = HeartbeatRequest> {
 }
 
 async fn streaming_heartbeat(client: &mut ChunkHandlerServiceClient<Channel>, num: usize) {
-    let in_stream = heartbeat_requests_iter().throttle(Duration::from_secs(1));
+    let interval = tokio::time::interval(Duration::from_millis(10));
+    let in_stream = IntervalStream::new(interval);
+    // let in_stream = heartbeat_requests_iter().throttle(Duration::from_secs(1));
 
     let stream = client.heartbeat(in_stream).await.unwrap().into_inner();
 
