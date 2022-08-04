63f9fd0cfc550dff1d1c44d3a5606426a2e5344e
lions-language <lions_language@163.com>
2022-08-04 10:08:41 +0800

    update

--- src/examples/tracing-test/src/main.rs
+++ src/examples/tracing-test/src/main.rs
@@ -5,7 +5,9 @@ use tracing_test::simple_file;
 fn main() {
     // basic::spans_test();
     // basic::event_test();
-    // simple_file::global_config_to_file();
+
     // custom_field::custom_field();
-    custom_field::use_local_time();
+    // custom_field::use_local_time();
+
+    simple_file::global_config_to_file();
 }
--- src/examples/tracing-test/src/simple_file/mod.rs
+++ src/examples/tracing-test/src/simple_file/mod.rs
@@ -1,19 +1,13 @@
 pub fn global_config_to_file() {
-    use tracing_subscriber::{fmt::format, prelude::*};
+    use tracing_subscriber::fmt::writer::MakeWriterExt;
 
-    // Format fields using the provided closure.
-    let format = format::debug_fn(|writer, field, value| {
-        // We'll format the field name and value separated with a colon.
-        write!(writer, "{}: {:?}", field, value)
-    })
-    // Separate each field with a comma.
-    // This method is provided by an extension trait in the
-    // `tracing-subscriber` prelude.
-    .delimited(", ");
+    let stdout = std::io::stdout.with_max_level(tracing::Level::INFO);
 
-    // Create a `fmt` subscriber that uses our custom event format, and set it
-    // as the default.
-    tracing_subscriber::fmt().fmt_fields(format).init();
+    let file_appender = tracing_appender::rolling::hourly(".logs/", "prefix.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::fmt()
+        .with_writer(stdout.and(non_blocking))
+        .init();
 
-    tracing::info!("preparing to shave yaks");
+    tracing::info!("hello");
 }
