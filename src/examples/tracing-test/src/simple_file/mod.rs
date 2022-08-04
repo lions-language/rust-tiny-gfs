70df44fcd8a76c21f3816ff4928f3e556b29fd13
lions-language <lions_language@163.com>
2022-08-04 10:22:04 +0800

    update

--- src/examples/tracing-test/src/simple_file/mod.rs
+++ src/examples/tracing-test/src/simple_file/mod.rs
@@ -6,7 +6,10 @@ pub fn global_config_to_file() {
     let file_appender = tracing_appender::rolling::hourly(".logs/", "prefix.log");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
     tracing_subscriber::fmt()
+        // merge stdio and file
         .with_writer(stdout.and(non_blocking))
+        // disenable ANSI terminal colors for formatted output
+        .with_ansi(false)
         .init();
 
     tracing::info!("hello");
