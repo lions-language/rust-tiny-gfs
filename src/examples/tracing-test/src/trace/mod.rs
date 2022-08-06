f7b8bc744b8b0b3556c24c9e114f8eebcb288c85
lions-language <lions_language@163.com>
2022-08-06 10:38:04 +0800

    update

--- src/examples/tracing-test/src/trace/mod.rs
+++ src/examples/tracing-test/src/trace/mod.rs
@@ -1,3 +1,5 @@
+use crate::basic::spans_record_test;
+
 pub fn print_trace() {
     use tracing_subscriber::fmt::writer::MakeWriterExt;
 
@@ -45,5 +47,38 @@ pub fn print_trace_v2() {
         tracing::event!(tracing::Level::INFO, "world");
 
         tracing::info!("my span");
+    }
+}
+
+pub fn print_trace_in_scope() {
+    use tracing::{info, info_span};
+    use tracing_subscriber::fmt::writer::MakeWriterExt;
+
+    let stdout = std::io::stdout.with_max_level(tracing::Level::INFO);
+
+    let file_appender = tracing_appender::rolling::hourly(".logs/", "prefix.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::fmt()
+        // merge stdio and file
+        .with_writer(stdout.and(non_blocking))
+        // disenable ANSI terminal colors for formatted output
+        .with_ansi(false)
+        .init();
+
+    {
+        let span = tracing::span!(tracing::Level::INFO, "my span");
+
+        span.in_scope(|| {
+            let inner_span = info_span!("hello_world");
+            // the span will be entered for the duration of the call to
+            // `hello_world`.
+            inner_span.in_scope(|| {
+                info!("hello 3");
+            });
+
+            info!("hello 1");
+        });
+
+        info!("hello 2");
     }
 }
