b9b31db9527352dfd33618125c66d842c4c7641a
lions-language <lions_language@163.com>
2022-08-08 21:55:22 +0800

    update

--- src/examples/tracing-test/src/macros/mod.rs
+++ src/examples/tracing-test/src/macros/mod.rs
@@ -14,3 +14,19 @@ pub fn use_debug_derive() {
     tracing::event!(tracing::Level::INFO, greeting = ?my_struct);
     // tracing::info!("hello");
 }
+
+pub fn use_display_derive() {
+    crate::init_stdout();
+
+    struct MyStruct {
+        field: &'static str,
+    }
+
+    let my_struct = MyStruct {
+        field: "Hello world!",
+    };
+
+    // `my_struct` will be recorded using its `fmt::Debug` implementation.
+    tracing::event!(tracing::Level::INFO, greeting = %my_struct);
+    // tracing::info!("hello");
+}
