c1ab696e7eda8a18e87236bc9ecfd4f133ccf4b5
lions-language <lions_language@163.com>
2022-08-15 21:40:37 +0800

    update

--- src/examples/tracing-test/src/library/mod.rs
+++ src/examples/tracing-test/src/library/mod.rs
@@ -16,6 +16,31 @@ pub fn create_appender_log<T>(name: &str, dir: &str, f: impl FnOnce() -> T) -> T
     tracing::subscriber::with_default(subscriber, f)
 }
 
+pub fn create_log<W, T>(w: W, f: impl FnOnce() -> T) -> T
+where
+    W: for<'writer> tracing_subscriber::fmt::MakeWriter<'writer> + 'static + Send + Sync,
+{
+    use tracing_subscriber::fmt::time::LocalTime;
+
+    let subscriber = tracing_subscriber::fmt()
+        .with_writer(w)
+        .with_max_level(tracing::Level::INFO)
+        .with_ansi(false)
+        .with_timer(LocalTime::rfc_3339())
+        .compact()
+        .finish();
+
+    tracing::subscriber::with_default(subscriber, f)
+}
+
+pub fn create_stdout_log<T>(f: impl FnOnce() -> T) -> T {
+    use tracing_subscriber::fmt::writer::MakeWriterExt;
+
+    let stdout = std::io::stdout.with_max_level(tracing::Level::INFO);
+
+    create_log(stdout, f)
+}
+
 pub fn test() {
     create_appender_log("log1", ".logs", || {
         tracing::info!("hello 1");
